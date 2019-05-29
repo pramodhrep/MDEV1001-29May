b17e32408ae746fca4987ac5c2b58c8c49183f84
Pramod Repaka <200411798@student.georgianc.on.ca>
2019-05-29 13:37:50 -0400

    Insert data to Teaches table

--- index.js
+++ index.js
@@ -17,4 +17,23 @@ db.serialize(function(){
     db.run("INSERT INTO Instructor VALUES(76766, 'Crick', 'Biology', 72000)");
     db.run("INSERT INTO Instructor VALUES(83821, 'Brandt', 'Comp. Sci.', 92000)");
     db.run("INSERT INTO Instructor VALUES(98345, 'Kim', 'Elec. Eng.', 80000)");
+
+    db.run("CREATE TABLE Teaches (ID NUMBER, Course_id TEXT, Sec_id NUMBER, Semester TEXT, Year NUMBER");
+
+    db.run("INSERT INTO Teaches VALUES(10101, 'CS-101', 1, 'Fall', 2009)");
+    db.run("INSERT INTO Teaches VALUES(10101, 'CS-315', 1, 'Spring', 2010)");
+    db.run("INSERT INTO Teaches VALUES(10101, 'CS-347', 1, 'Fall', 2019)");
+    db.run("INSERT INTO Teaches VALUES(12121, 'FIN-201', 1, 'Spring', 2009)");
+    db.run("INSERT INTO Teaches VALUES(15151, 'MU-199', 1, 'Spring', 2010)");
+    db.run("INSERT INTO Teaches VALUES(22222, 'PHY-101', 1, 'Fall', 2009)");
+    db.run("INSERT INTO Teaches VALUES(23343, 'HIS-351', 1, 'Spring', 2010)");
+    db.run("INSERT INTO Teaches VALUES(45565, 'CS-101', 1, 'Spring', 2009)");
+    db.run("INSERT INTO Teaches VALUES(45565, 'CS-319', 1, 'Spring', 2010)");
+    db.run("INSERT INTO Teaches VALUES(76766, 'BIO-101', 1, 'Summer', 2009)");
+    db.run("INSERT INTO Teaches VALUES(76766, 'BIO-301', 1, 'Summer', 2010)");
+    db.run("INSERT INTO Teaches VALUES(83821, 'CS-190', 1, 'Spring', 2009)");
+    db.run("INSERT INTO Teaches VALUES(83821, 'CS-190', 2, 'Spring', 2009)");
+    db.run("INSERT INTO Teaches VALUES(83821, 'CS-319', 1, 'Spring', 2010)");
+    db.run("INSERT INTO Teaches VALUES(98345, 'EE-181, 1, 'Spring', 2009)");
+
 });
